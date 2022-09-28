a6fca6a2346549374789ddf8383482356adc680d
Victor Canto <iamvictorcanto@gmail.com>
2022-09-27 21:06:50 -0300

    feat: finished App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar as RNStatusBar,
+} from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Focus } from './src/features/Focus';
+import { FocusHistory } from './src/features/FocusHistory';
+import { Timer } from './src/features/Timer';
+import { colors } from './src/theme';
 
 export default function App() {
+  const [currentSubject, setCurrentSubject] = useState<string | null>(null);
+  const [history, setHistory] = useState<string[]>([]);
+
+  const onTimerEnd = (subject: string | null) => {
+    setHistory([...history, subject as string]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        {!currentSubject ? (
+          <>
+            <Focus addSubject={setCurrentSubject} />
+            <FocusHistory history={history} />
+          </>
+        ) : (
+          <Timer
+            focusSubject={currentSubject}
+            onTimerEnd={onTimerEnd}
+            clearSubject={() => setCurrentSubject(null)}
+          />
+        )}
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? RNStatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
   },
 });
